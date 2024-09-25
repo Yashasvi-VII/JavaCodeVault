609e84f55e46beb4cfb7d4ef6316c6f7d3554ec1
Yashasvi <98901285+Yashasvi-VII@users.noreply.github.com>
2024-09-25 18:50:16 +0530

    Completed data types and variables practice

---
+++ DataTypesAndVariables/src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        int myNumber = 1929; // initialization
+        int yourNumber; // declaration
+        System.out.println(myNumber);
+
+        boolean isVegetarian = true;
+        System.out.println("Is vegeterian "+ isVegetarian);
+
+        float myfloat = 0.9f;
+        System.out.println(myfloat);
+
+        double myDouble = 0.9282;
+        System.out.println(myDouble);
+
+        String myString = "Good morning";
+        System.out.println(myString);
+    }
+}
