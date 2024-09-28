04257e48e1d32077b5cfb8b489167c8e0bee33e5
Yashasvi-VII <yashasvi9987@gmail.com>
2024-09-28 12:53:37 +0530

    Completed first programming challenge

---
+++ programmingChallenge/src/challenge01/Patterns.java
@@ -0,0 +1,18 @@
+package challenge01;
+
+public class Patterns {
+    public static void main(String[] args) {
+        System.out.println("Right half pyramid");
+        System.out.println("*\n*\t*\n*\t*\t*\n*\t*\t*\t*\n*\t*\t*\t*\t*");
+
+        System.out.println();
+
+        System.out.println("Reverse half pyramid.");
+        System.out.println("*\t*\t*\t*\t*\n*\t*\t*\t*\n*\t*\t*\n*\t*\n*");
+
+        System.out.println();
+
+        System.out.println("Left half pyramid.");
+        System.out.println("\t\t\t\t*\n\t\t\t*\t*\n\t\t*\t*\t*\n\t*\t*\t*\t*\n*\t*\t*\t*\t*");
+    }
+}
