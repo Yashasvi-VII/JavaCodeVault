7cd425052afbd4e2d90bc76fde01b9fd27c94984
Yashasvi-VII <yashasvi9987@gmail.com>
2024-10-01 14:52:14 +0530

    completed swap two numbers

--- programmingChallenge/src/challenge03/swapNumbers.java
+++ programmingChallenge/src/challenge03/swapNumbers.java
@@ -1,7 +1,24 @@
 package challenge03;
 
+import java.util.Scanner;
+
 public class swapNumbers {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        System.out.print("Please enter any whole number ");
+        int firstNumber = sc.nextInt();
+
+        System.out.print("Please enter one more whole number ");
+        int secondNumber = sc.nextInt();
+
+        System.out.println("Numbers before swapping FirstNumber: "+firstNumber +" and SecondNumber: "+secondNumber);
+
+        int temp = 0;
+        temp = firstNumber;
+        firstNumber = secondNumber;
+        secondNumber = temp;
+
+        System.out.println("Numbers before swapping FirstNumber: "+firstNumber +" and SecondNumber: "+secondNumber);
     }
 }
