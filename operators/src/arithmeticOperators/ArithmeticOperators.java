b82acf5324b349fd1757eec92e84f8992eedce6b
Yashasvi-VII <yashasvi9987@gmail.com>
2024-10-01 15:07:44 +0530

    completed arithmetic operators

---
+++ operators/src/arithmeticOperators/ArithmeticOperators.java
@@ -0,0 +1,23 @@
+package arithmeticOperators;
+
+public class ArithmeticOperators {
+    public static void main(String[] args) {
+
+        int a=8;
+        int b=9;
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+
+        double x=9;
+        double y=7;
+        System.out.println(x+y);
+        System.out.println(x-y);
+        System.out.println(x*y);
+        System.out.println(x/y);
+
+        // modulus operator only works in integer
+        System.out.println(a%b);
+    }
+}
