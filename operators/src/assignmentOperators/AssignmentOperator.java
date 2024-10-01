b97c79c5968eb9c9c0f0f1ccd18dda2db439a60a
Yashasvi-VII <yashasvi9987@gmail.com>
2024-10-01 14:48:36 +0530

    Done first operators

---
+++ operators/src/assignmentOperators/AssignmentOperator.java
@@ -0,0 +1,15 @@
+package assignmentOperators;
+
+public class AssignmentOperator {
+    public static void main(String[] args) {
+
+        // = is assignment operator.
+
+        int evenNumber = 2;
+        int oddNumber = 3;
+
+        //This is wrong and will give error
+        // 9 = oddNumber;
+
+    }
+}
---
+++ programmingChallenge/src/challenge03/swapNumbers.java
@@ -0,0 +1,7 @@
+package challenge03;
+
+public class swapNumbers {
+    public static void main(String[] args) {
+
+    }
+}
